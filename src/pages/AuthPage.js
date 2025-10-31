1d7633c0cd61fb831733f739231490e8c0d92655
Aleti-Bhavana <bhavanaaleti8897@gmail.com>
2025-10-31 18:52:46 +0530

    Update login API endpoint to production URL

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -28,7 +28,7 @@ const AuthPage = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/v1/auth/login", {
+      const res = await axios.post("https://authrole-backend.onrender.com/api/v1/auth/login", {
         username,
         password,
       });
